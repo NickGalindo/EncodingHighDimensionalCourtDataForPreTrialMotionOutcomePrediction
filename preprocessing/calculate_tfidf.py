cd8850cbf88d333892c90a22308a62defd27ff47
NickGalindo <nickdavga@gmail.com>
2025-01-13 16:53:22 -0500

    brrr

--- preprocessing/calculate_tfidf.py
+++ preprocessing/calculate_tfidf.py
@@ -8,6 +8,16 @@ train_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/mapped
 val_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/mapped_full_val.csv")
 test_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/mapped_full_test.csv")
 
-corpus = set(train_data["filepath"].dropna().unique().tolist() + val_data["filepath"].dropna().unique().tolist() + test_data["filepath"].dropna().unique().tolist())
+corpus = set(train_data["filepath"].dropna().unique().tolist() + val_data["filepath"].dropna().unique().tolist() + test_data["filepath"].dropna().unique().tolist()) # type: ignore
 
-print(corpus)
+full_corpus = []
+
+for doc_path in corpus:
+    with open(doc_file, "r") as file: #type:ignore
+        full_corpus.append(file.read())
+
+
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_vectorizer.fit(full_corpus)
+
+
