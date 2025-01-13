5bbedd4f445ba4180867b80a08bffe5dc3ef1ada
NickGalindo <nickdavga@gmail.com>
2025-01-13 00:55:38 -0500

    brrr

--- preprocessing/join_text_tabular_data.py
+++ preprocessing/join_text_tabular_data.py
@@ -1,39 +1,35 @@
 import pandas as pd
 import numpy as np
 
-tabular_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/motionStrike_ALLcodes_data.tsv", sep="\t")
+
+train_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/full_train.csv")
+val_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/full_val.csv")
+test_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/full_test.csv")
+
+
 text_data_paths = pd.read_csv("/mnt/research/aguiarlab/proj/law/pdfs/all_txt_files_abs_path.txt", names=["filepath"], header=None)
 mapping_DocumentNo_CaseRefNum = pd.read_csv("/mnt/research/aguiarlab/proj/law/jz_script/judcaseid_docid_translationtable.tsv", sep="\t")
 
 
-print(f"tabular_data shape: {tabular_data.shape}")
-print(f"text_data_paths shape: {text_data_paths.shape}")
-print(f"mapping_DocumentNo_CaseRefNum shape: {mapping_DocumentNo_CaseRefNum.shape}")
+full_mapper = pd.merge(mapping_DocumentNo_CaseRefNum, text_data_paths, left_on="DocumentNo ", right_on="document_no", how="left")
 
 
-text_data_paths["document_no"] = text_data_paths["filepath"].str.extract(r"(\d+)(?!.*\d)")
-text_data_paths = text_data_paths.dropna()
-text_data_paths["document_no"] = text_data_paths["document_no"].astype(int)
+print(f"shape train_data:\t\t{train_data.shape}")
+print(f"shape val_data:\t\t{val_data.shape}")
+print(f"shape test_data:\t\t{test_data.shape}")
 
-print(f"text_data_paths shape after building DocumentNo: {text_data_paths.shape}")
+print(f"Number of CaseReferenceNumber without file:\t{full_mapper['filepath'].isna().sum()}")
 
-case_ref_num_not_in_mapping = set(tabular_data["CaseReferenceNumber"].tolist()) - set(mapping_DocumentNo_CaseRefNum["CaseRefNum "].tolist())
-document_no_not_in_mapping = set(text_data_paths["document_no"].tolist()) - set(mapping_DocumentNo_CaseRefNum["DocumentNo"].tolist())
+train_data_full = pd.merge(train_data, full_mapper, left_on="CaseReferenceNumber", right_on="CaseRefNum", how="left")
+val_data_full = pd.merge(val_data, full_mapper, left_on="CaseReferenceNumber", right_on="CaseRefNum", how="left")
+test_data_full = pd.merge(test_data, full_mapper, left_on="CaseReferenceNumber", right_on="CaseRefNum", how="left")
 
-print(f"Amount of case reference numbers in tabular data not in the judcaseid_docid_translationtable: {len(case_ref_num_not_in_mapping)}")
-print(f"Amount of document no in text data not in the judcaseid_docid_translationtable: {len(document_no_not_in_mapping)}")
+print(f"shape mapped train data:\t\t{train_data_full.shape}")
+print(f"shape mapped val data:\t\t{val_data_full.shape}")
+print(f"shape mapped test data:\t\t{test_data_full.shape}")
 
+print()
 
-full_data_with_documentNo = pd.merge(tabular_data, mapping_DocumentNo_CaseRefNum, left_on="CaseReferenceNumber", right_on="CaseRefNum ", how="inner")
-a = full_data_with_documentNo.copy()
-
-print(f"left join on tabular data and the document no to case reference number mapping shape: {full_data_with_documentNo.shape}")
-
-full_data_with_documentNo = pd.merge(full_data_with_documentNo, text_data_paths, left_on="DocumentNo", right_on="document_no", how="inner")
-b = full_data_with_documentNo.copy()
-
-
-print(f"left join on previous calculated tabular data with document no on the text data and their numbers to correlate filepaths with tabular data shape: {full_data_with_documentNo.shape}")
-
-
-print(full_data_with_documentNo["CaseReferenceNumber"][full_data_with_documentNo["CaseReferenceNumber"].duplicated()].unique())
+print(f"mapped train data without file:\t{train_data_full['filepath'].isna().sum()}")
+print(f"mapped val data without file:\t{val_data_full['filepath'].isna().sum()}")
+print(f"mapped test data without file:\t{test_data_full['filepath'].isna().sum()}")
