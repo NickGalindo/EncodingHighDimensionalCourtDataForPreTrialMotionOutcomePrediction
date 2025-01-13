1e10a58a075b9f915e5bde9f6b3da32fc046b9de
NickGalindo <nickdavga@gmail.com>
2025-01-13 00:40:11 -0500

    brrr

--- preprocessing/join_text_tabular_data.py
+++ preprocessing/join_text_tabular_data.py
@@ -23,30 +23,17 @@ document_no_not_in_mapping = set(text_data_paths["document_no"].tolist()) - set(
 print(f"Amount of case reference numbers in tabular data not in the judcaseid_docid_translationtable: {len(case_ref_num_not_in_mapping)}")
 print(f"Amount of document no in text data not in the judcaseid_docid_translationtable: {len(document_no_not_in_mapping)}")
 
-print(tabular_data.columns)
-print(mapping_DocumentNo_CaseRefNum.columns)
 
 full_data_with_documentNo = pd.merge(tabular_data, mapping_DocumentNo_CaseRefNum, left_on="CaseReferenceNumber", right_on="CaseRefNum ", how="inner")
 a = full_data_with_documentNo.copy()
-
-print(full_data_with_documentNo.columns)
 
 print(f"left join on tabular data and the document no to case reference number mapping shape: {full_data_with_documentNo.shape}")
 
 full_data_with_documentNo = pd.merge(full_data_with_documentNo, text_data_paths, left_on="DocumentNo", right_on="document_no", how="inner")
 b = full_data_with_documentNo.copy()
 
-print(text_data_paths.columns)
-print(full_data_with_documentNo.columns)
 
 print(f"left join on previous calculated tabular data with document no on the text data and their numbers to correlate filepaths with tabular data shape: {full_data_with_documentNo.shape}")
 
-print(set(a.columns)-set(b.columns))
-print(set(b.columns)-set(a.columns))
-print(a.columns)
-print(b.columns)
 
-mismatch = pd.merge(a, b, on=a.columns, how="outer", indicator=True)
-extra = mismatch[mismatch["_merge"] != "both"]
-
-print(extra)
+print(full_data_with_documentNo["CaseReferenceNumber"].duplicated())
