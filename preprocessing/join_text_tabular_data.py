7e5e9ada0d66982743d26efdd1ebbe2c6db8c449
NickGalindo <nickdavga@gmail.com>
2025-01-13 01:03:23 -0500

    brrr

--- preprocessing/join_text_tabular_data.py
+++ preprocessing/join_text_tabular_data.py
@@ -18,16 +18,20 @@ full_mapper = pd.merge(mapping_DocumentNo_CaseRefNum, text_data_paths, left_on="
 
 
 print(f"shape train_data:\t\t{train_data.shape}")
-print(f"shape val_data:\t\t{val_data.shape}")
+print(f"shape val_data:\t\t\t{val_data.shape}")
 print(f"shape test_data:\t\t{test_data.shape}")
 
+print()
+
 print(f"Number of CaseReferenceNumber without file:\t{full_mapper['filepath'].isna().sum()}")
+
+print()
 
 train_data_full = pd.merge(train_data, full_mapper, left_on="CaseReferenceNumber", right_on="CaseRefNum ", how="left")
 val_data_full = pd.merge(val_data, full_mapper, left_on="CaseReferenceNumber", right_on="CaseRefNum ", how="left")
 test_data_full = pd.merge(test_data, full_mapper, left_on="CaseReferenceNumber", right_on="CaseRefNum ", how="left")
 
-print(f"shape mapped train data:\t\t{train_data_full.shape}")
+print(f"shape mapped train data:\t{train_data_full.shape}")
 print(f"shape mapped val data:\t\t{val_data_full.shape}")
 print(f"shape mapped test data:\t\t{test_data_full.shape}")
 
