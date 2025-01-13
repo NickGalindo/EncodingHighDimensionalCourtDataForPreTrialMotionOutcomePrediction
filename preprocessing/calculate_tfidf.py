5f2315028cb117f52b7f188b4d443e8e69dcf154
NickGalindo <nickdavga@gmail.com>
2025-01-13 14:45:48 -0500

    brrr

--- preprocessing/calculate_tfidf.py
+++ preprocessing/calculate_tfidf.py
@@ -4,4 +4,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import pandas as pd
 
+train_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/mapped_full_train.csv")
+val_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/mapped_full_val.csv")
+test_data = pd.read_csv("/mnt/research/aguiarlab/proj/law/data/PaperData/mapped_full_test.csv")
 
+corpus = set(train_data["filepath"].unique().tolist() + val_data["filepath"].unique().tolist() + test_data["filepath"].unique().tolist())
