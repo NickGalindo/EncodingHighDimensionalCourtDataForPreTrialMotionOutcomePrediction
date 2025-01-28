dc32ccdc38f7747620dd770cc4cbcbf502b888b3
NickGalindo <nickdavga@gmail.com>
2025-01-27 21:00:02 -0500

    lil nas

--- train/bert_train_gpu.py
+++ train/bert_train_gpu.py
@@ -42,7 +42,7 @@ test_data = pd.read_csv(os.path.join(base_dir, "mapped_full_test.csv"))
 test_data = test_data[["filepath", "document_no", "MotionResultCode"]]
 test_data["label"] = test_data["MotionResultCode"].apply(lambda x: 1 if x == "GR" else 0)
 
-full_corpus = pickle.load(open(os.path.join(os.path.join(base_dir, "textData/tfidf"), "indexed_text.pkl"), "rb"))
+full_corpus = pickle.load(open(os.path.join(os.path.join(base_dir, "textData/tfigm"), "indexed_text.pkl"), "rb"))
 
 train_data["text"] = train_data["document_no"].map(full_corpus)
 val_data["text"] = val_data["document_no"].map(full_corpus)
@@ -132,8 +132,8 @@ accuracy = accuracy_score(labels, pred_classes)
 
 print(f"ACCURACY ON TEST: {accuracy}")
 
-model_path = os.path.join(base_dir, "bertTraining/models/tfidf/extra/model")
-tok_path = os.path.join(base_dir, "bertTraining/models/tfidf/extra/tokenizer")
+model_path = os.path.join(base_dir, "bertTraining/models/tfigm/extra/model")
+tok_path = os.path.join(base_dir, "bertTraining/models/tfigm/extra/tokenizer")
 os.makedirs(model_path, exist_ok=True)
 os.makedirs(tok_path, exist_ok=True)
 model.save_pretrained(model_path);
