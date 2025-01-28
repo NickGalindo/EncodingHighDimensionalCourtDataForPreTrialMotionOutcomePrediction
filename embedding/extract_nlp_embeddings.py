a139f7ca4e715fa7516d826484f066a41c39ff92
NickGalindo <nickdavga@gmail.com>
2025-01-28 03:44:30 -0500

    >.<

--- embedding/extract_nlp_embeddings.py
+++ embedding/extract_nlp_embeddings.py
@@ -32,7 +32,7 @@ test_data["label"] = test_data["MotionResultCode"].apply(lambda x: 1 if x == "GR
 
 
 
-full_corpus = pickle.load(open(os.path.join(os.path.join(base_dir, "textData/alltext"), "indexed_text.pkl"), "rb"))
+full_corpus = pickle.load(open(os.path.join(os.path.join(base_dir, "textData/tfidf"), "indexed_text.pkl"), "rb"))
 
 
 
@@ -45,7 +45,7 @@ val_data = val_data[["text", "label", "MotionID"]].dropna().reset_index()
 test_data = test_data[["text", "label", "MotionID"]].dropna().reset_index()
 
 
-model_path = "/home/coder/second/PaperData/bertTraining/models/alltext/extra"
+model_path = "/home/coder/second/PaperData/bertTraining/models/tfidf/extra"
 tokenizer = BertTokenizer.from_pretrained(os.path.join(model_path, "tokenizer"))
 model = BertForSequenceClassification.from_pretrained(os.path.join(model_path, "model")).bert
 
@@ -96,7 +96,7 @@ train_embedding_dict = relateEmbeddingToMotionID(train_embedding, train_data)
 val_embedding_dict = relateEmbeddingToMotionID(val_embedding, val_data)
 test_embedding_dict = relateEmbeddingToMotionID(test_embedding, test_data)
 
-save_path = "/home/coder/second/PaperData/nlp_embeddings/bert_truncation"
+save_path = "/home/coder/second/PaperData/nlp_embeddings/bert_tfidf"
 os.makedirs(save_path, exist_ok=True)
 with open(os.path.join(save_path, "train_embedding.pkl"), "wb") as file:
     pickle.dump(train_embedding, file)
